7b808781227b44d6735d53bfe0e9fb5e2d64255b
artemasver3-2 <hgunter22206@gmail.com>
2024-10-03 07:01:35 -0500

    y

--- src/app/header.js
+++ src/app/header.js
@@ -10,7 +10,12 @@ flex-direction: column;
 align-items: center;
 justify-content: center;
 border-bottom: 2px solid #62AB37;
-
+transition: 1s ease-in;
+&:hover{
+  color: #980002;
+  text-decoration: line-through;
+  opacity: 1;
+} 
 
 
 @media only screen and (max-width : 460px)  {
@@ -25,13 +30,6 @@ font-family: Bebas Neue;
 color: #62ab37;
 text-decoration: none;
 margin-top: .5rem;
-transition: 1s ease-in;
-&:hover{
-  color: #980002;
-  text-decoration: line-through;
-  opacity: 1;
-} 
-
 
 @media only screen and (max-width : 460px)  {
   font-size: 2.5rem;
