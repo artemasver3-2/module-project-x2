5914e1ab2b130c1011a1f8074491363fcbb78fec
artemasver3-2 <hgunter22206@gmail.com>
2024-10-05 10:22:47 -0500

    hm

--- src/fainne/fainneuron.js
+++ src/fainne/fainneuron.js
@@ -1,144 +1,145 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import FainneCard from './fainnecard';
 import './fainne.css';
 import Nav from '../app/nav';
 
-
 const StyledHeader = styled.header`
-background-color: #000000;
-min-height: 15vh;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-border-bottom: 2px solid #A71515;
-
-@media only screen and (max-width : 460px)  {
+  background-color: #000000;
   min-height: 15vh;
-  margin-bottom: .5rem;
-};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 2px solid #a71515;
+
+  @media only screen and (max-width: 460px) {
+    min-height: 15vh;
+    margin-bottom: 0.5rem;
+  }
 `;
 
 const StyledTitle = styled.h1`
-font-size: 4.5rem;
-font-family: Bebas Neue;
-color: #A71515;
-text-decoration: none;
-letter-spacing: rem;
-margin-top: 1rem;
-transition: 1s ease-in-out;
-&:hover{
-  color: #062514;
-  opacity: 1;
-} 
+  font-size: 4.5rem;
+  font-family: Bebas Neue;
+  color: #a71515;
+  text-decoration: none;
+  letter-spacing: rem;
+  margin-top: 1rem;
+  transition: 1s ease-in-out;
+  background: linear-gradient(to right, a71515 50%, white 062514%);
+  background-size: 200% 100%;
+  background-position: right bottom;
+  &:hover {
+    opacity: 1;
+  }
 
-@media only screen and (max-width : 460px)  {
-  font-size: 2.5rem;
-  margin-bottom: 3.75rem;
-  margin-top: 2px;
-};
+  @media only screen and (max-width: 460px) {
+    font-size: 2.5rem;
+    margin-bottom: 3.75rem;
+    margin-top: 2px;
+  }
 `;
 
 const StyledSubtitle = styled.h2`
-font-size: 2rem;
-font-style: italic;
-font-family: Kristi;
-color: white;
-margin-top: -4.5rem;
-margin-left: 20rem;
-letter-spacing: 2px;
+  font-size: 2rem;
+  font-style: italic;
+  font-family: Kristi;
+  color: white;
+  margin-top: -4.5rem;
+  margin-left: 20rem;
+  letter-spacing: 2px;
 
-@media only screen and (max-width : 460px)  {
-  font-size: 1.25rem;
-  margin-left: 4rem;
-};
+  @media only screen and (max-width: 460px) {
+    font-size: 1.25rem;
+    margin-left: 4rem;
+  }
 `;
 
 const StyledFooter = styled.footer`
-background-color: #000000;
-min-height: 15vh;
-border-top: 2px solid #A71515;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+  background-color: #000000;
+  min-height: 15vh;
+  border-top: 2px solid #a71515;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-@media only screen and (max-width : 460px)  {
-  text-align: center;
-  min-height: 10vh;
- };
+  @media only screen and (max-width: 460px) {
+    text-align: center;
+    min-height: 10vh;
+  }
 `;
 
 const StyledFooterSpan1 = styled.span`
-font-size: 1.75rem;
-font-family: Bebas Neue;
-color: white;
-letter-spacing: 2px;
+  font-size: 1.75rem;
+  font-family: Bebas Neue;
+  color: white;
+  letter-spacing: 2px;
 
-@media only screen and (max-width : 460px)  {
-  font-size: 1rem;
-  margin-bottom: .5rem;
-  margin-top: -1rem;
-};
+  @media only screen and (max-width: 460px) {
+    font-size: 1rem;
+    margin-bottom: 0.5rem;
+    margin-top: -1rem;
+  }
 `;
 
 const StyledFooterSpan2 = styled.span`
-font-size: 2rem;
-font-family: Pacifico;
-color: #A71515;
-letter-spacing: 2px;
-margin-top: -12px;
-margin-bottom:-1rem;
+  font-size: 2rem;
+  font-family: Pacifico;
+  color: #a71515;
+  letter-spacing: 2px;
+  margin-top: -12px;
+  margin-bottom: -1rem;
 
-@media only screen and (max-width : 460px)  {
-  font-size: 1rem;
- };
+  @media only screen and (max-width: 460px) {
+    font-size: 1rem;
+  }
 `;
-
 
 export default class FainneUron extends React.Component {
   constructor(props) {
-    super(props) 
-    this.state = {
-
-    }
+    super(props);
+    this.state = {};
   }
   render() {
     return (
-        <div className='fainne-page-div'>
-      <StyledHeader>
-        <StyledTitle>The Outsiders</StyledTitle>
-        <StyledSubtitle>Ebixia's most wanted heros</StyledSubtitle>
-        <Nav />
-      </StyledHeader>
+      <div className="fainne-page-div">
+        <StyledHeader>
+          <StyledTitle>The Outsiders</StyledTitle>
+          <StyledSubtitle>Ebixia's most wanted heros</StyledSubtitle>
+          <Nav />
+        </StyledHeader>
 
-       <div className='fainne-body'>
-        <FainneCard 
-         title="Fainne Uron"
-         subtitle="strong, you know, like a tree"
-         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. "
-        />
+        <div className="fainne-body">
+          <FainneCard
+            title="Fainne Uron"
+            subtitle="strong, you know, like a tree"
+            info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. "
+          />
 
-        <FainneCard 
-         title="The Mighty Timberlands"
-         subtitle="something something?"
-         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. "
-        />
-       </div>
+          <FainneCard
+            title="The Mighty Timberlands"
+            subtitle="something something?"
+            info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. "
+          />
+        </div>
 
-       <div className='fainne-body'>
-        <FainneCard 
-         title="Forest Gremlin"
-         subtitle="why so weird?"
-         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. "
-        />
-       </div>
+        <div className="fainne-body">
+          <FainneCard
+            title="Forest Gremlin"
+            subtitle="why so weird?"
+            info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. "
+          />
+        </div>
 
-       <StyledFooter>
-            <StyledFooterSpan1>Outsider NewsLetter Property of:</StyledFooterSpan1><StyledFooterSpan2>Eleanor Dragon</StyledFooterSpan2>
-       </StyledFooter>
-       </div>
+        <StyledFooter>
+          <StyledFooterSpan1>
+            Outsider NewsLetter Property of:
+          </StyledFooterSpan1>
+          <StyledFooterSpan2>Eleanor Dragon</StyledFooterSpan2>
+        </StyledFooter>
+      </div>
     );
   }
 }
