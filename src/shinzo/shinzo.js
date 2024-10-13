b4199496dc91168ce44ee4224581ca07a3f24f01
artemasver3-2 <hgunter22206@gmail.com>
2024-10-12 21:16:19 -0500

    c

--- src/shinzo/shinzo.js
+++ src/shinzo/shinzo.js
@@ -36,19 +36,18 @@ perspective: 500px;
     transform: translate(-50%, -50%);
     text-shadow: 0.01em 0.01em 0.01em rgba(0, 0, 0, 0.3);
 }
-/* &:before {
+&:before {
     animation: floatAbove 3.5s ease-in-out infinite;
     -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
             clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
 }
 &:after {
-    opacity: 0.65;
-    filter: blur(0.20em);
+    
     transform: translate(-50%, -50%) rotateX(21deg);
     animation: floatBelow 3.5s ease-in-out infinite;
     -webkit-clip-path: polygon(0% 50%, 100% 50%, 100% 100%, 0% 100%);
             clip-path: polygon(0% 50%, 100% 50%, 100% 100%, 0% 100%);
-} */
+}
 
 
 @keyframes floatAbove {
@@ -64,6 +63,8 @@ perspective: 500px;
         transform: translate(-50%, -60%) rotateX(10deg);
         -webkit-clip-path: polygon(0% 60%, 100% 60%, 100% 100%, 0% 100%);
                 clip-path: polygon(0% 60%, 100% 60%, 100% 100%, 0% 100%);
+                opacity: 0.65;
+                filter: blur(0.20em);
     }
 }
 
