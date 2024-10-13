1e5060dc2e63ff22a02f14f333ae82bc563dbfaa
artemasver3-2 <hgunter22206@gmail.com>
2024-10-12 20:38:31 -0500

    n

--- src/shinzo/shinzo.js
+++ src/shinzo/shinzo.js
@@ -21,17 +21,14 @@ border-bottom: 2px solid #011120;
 
 const StyledTitle = styled.h1`
 font-family: Bebas Neue;
+font-size: 4.5rem;
 color: #011120;
 margin-top: 1rem;
 position: relative;
-font-size: calc(10px + 10vw);
-font-weight: 700;
-color: #f5f5f5;
-letter-spacing: 0.02em;
 text-transform: uppercase;
 perspective: 500px;
-&::before,
-::after {
+&:before,
+:after {
     content: attr(aria-label);
     position: absolute;
     top: 0;
@@ -39,13 +36,13 @@ perspective: 500px;
     transform: translate(-50%, -50%);
     text-shadow: 0.01em 0.01em 0.01em rgba(0, 0, 0, 0.3);
 }
-&::before {
+&:before {
     animation: floatAbove 3.5s ease-in-out infinite;
     -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
             clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
 }
 &
-::after {
+:after {
     opacity: 0.65;
     filter: blur(0.02em);
     transform: translate(-50%, -50%) rotateX(21deg);
