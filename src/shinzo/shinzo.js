c63a3b0ae1621fd1a91ebdc78043932e0580854c
artemasver3-2 <hgunter22206@gmail.com>
2024-10-12 21:41:09 -0500

    idk fr

--- src/shinzo/shinzo.js
+++ src/shinzo/shinzo.js
@@ -39,14 +39,31 @@ const StyledTitle = styled.h1`
     -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
             clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
 }
-&&:after {
+&::after {
   content: attr(aria-label);
-    opacity: 0.95;
-    transform: translate(-50%, -50%), rotateX(21deg);
+  opacity: 0.65;
+    filter: blur(0.02em);
+    transform: translate(-50%, -50%) rotateX(21deg);
+    animation: floatBelow 3.5s ease-in-out infinite;
     -webkit-clip-path: polygon(0% 50%, 100% 50%, 100% 100%, 0% 100%);
             clip-path: polygon(0% 50%, 100% 50%, 100% 100%, 0% 100%);
 }
 
+@keyframes floatAbove {
+    50% {
+        transform: translate(-50%, -60%);
+        -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 60%, 0% 60%);
+                clip-path: polygon(0% 0%, 100% 0%, 100% 60%, 0% 60%);
+    }
+}
+
+@keyframes floatBelow {
+    50% {
+        transform: translate(-50%, -60%) rotateX(10deg);
+        -webkit-clip-path: polygon(0% 60%, 100% 60%, 100% 100%, 0% 100%);
+                clip-path: polygon(0% 60%, 100% 60%, 100% 100%, 0% 100%);
+    }
+}
 
 
 @media only screen and (max-width : 460px)  {
