b82c0fed803d4a6a7f2c706aa54b096998e6ccfc
artemasver3-2 <hgunter22206@gmail.com>
2024-10-12 21:11:43 -0500

    ccccc

--- src/shinzo/shinzo.js
+++ src/shinzo/shinzo.js
@@ -27,8 +27,8 @@ margin-top: 3rem;
 margin-bottom: 7rem;
 text-transform: uppercase;
 perspective: 500px;
-&::before,
-::after {
+&:before,
+:after {
     content: attr(aria-label);
     position: absolute;
     top: 0;
@@ -36,13 +36,13 @@ perspective: 500px;
     transform: translate(-50%, -50%);
     text-shadow: 0.01em 0.01em 0.01em rgba(0, 0, 0, 0.3);
 }
-&::before {
+&:before {
     animation: floatAbove 3.5s ease-in-out infinite;
     -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
             clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%);
 }
 &
-::after {
+:after {
     opacity: 0.65;
     filter: blur(0.20em);
     transform: translate(-50%, -50%) rotateX(21deg);
