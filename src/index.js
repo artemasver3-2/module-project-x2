0631ba1b15734e28ba72ca15fdd6c5f45d5922e7
artemasver3-2 <hgunter22206@gmail.com>
2024-10-23 22:08:04 -0500

    iwaszoomedinitisbedtimegn

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './app/App';
 import { HashRouter } from 'react-router-dom';
 
-const scale = 'scale(1)';
-document.body.style.Transform = scale; // Chrome, Opera, Safari
-document.body.style.msTransform = scale; // IE 9
-document.body.style.transform = scale; // General
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
