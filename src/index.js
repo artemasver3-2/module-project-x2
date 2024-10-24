a31e82fe4a328dde59c39a76ad7e0abbbfd73f5f
artemasver3-2 <hgunter22206@gmail.com>
2024-10-23 22:01:20 -0500

    this maybe?

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 
+const scale = 'scale(1)';
+document.body.style.Transform = scale; // Chrome, Opera, Safari
+document.body.style.msTransform = scale; // IE 9
+document.body.style.transform = scale; // General
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter>
-     <App />
-    </HashRouter>
-)
-
-
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
