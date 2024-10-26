b4c494a698301c0c048598f3e64ed968ae0dab57
artemasver3-2 <hgunter22206@gmail.com>
2024-10-26 17:01:41 -0500

    holly page

--- src/holly/holly.js
+++ src/holly/holly.js
@@ -24,9 +24,9 @@ export default class Holly extends React.Component {
         <HollyCard 
          title="Hollyanna Knox"
          subtitle="duchess... among other things."
-         info={<>Well, hi... I guess this is supposed to be where I write a little bit about myself? I'm not good at these things. I didn't have your traditional learning experience. I didn’t really have your traditional upbringing, to be fair. I was born in a small village outside of Banetrail, went to school there actually, which would be appoint of pride if there weren’t kind of di– 
-         
-          I’ve been told I can’t say that here. Either way, let’s just say, even if I hold the title of Duchess and hero these days, I have a simple set of roots, slightly rotted, but they are spread through Rock’s Hollow and Banetrail enough that I can call both of them mine in a way. I did have a home in both places, but I gave my childhood home away recently. I’m babbling, sorry… 
+         info={<>Well, hi... I guess this is supposed to be where I talk a little bit about myself? I'm not good at these things. I didn't have your traditional learning experience, which I hear is where you learn to properly introduce yourself. I didn’t really have your traditional upbringing, to be fair. I was born in a small village outside of Banetrail, went to school there actually, which would be appoint of pride if there weren’t kind of di– 
+
+          I’ve been told I can’t say that here. Either way, let’s just say, even if I hold the title of Duchess and hero these days, I have a simple set of roots, slightly rotted, but they are spread through Rock’s Hollow and Banetrail enough that I can call both of them <text className='italics'>mine</text> in a way. I did have a home in both places, but I gave my childhood home away recently. I’m babbling, sorry… 
         
           I don’t think we should talk much about my childhood, but I do think I am supposed to be introducing myself, so, these days I live mostly in [redacted] with the other Outsider’s. It’s a nice little spot, lots of [redacted], and [redacted]. I enjoy my time there as well as the time I spend in Banetrail at my place there. Banetrail has been so welcoming and they seem to be proud of me, which is not something I can say I’ve had a lot in my life, so I’m happy to be Hollyanna Knox, hero of the land, Banetrail’s own Duchess/Academic. Teaching is not something I would have ever seen myself as capable of growing up, but I thin we’ve all come a long way, separately and as a team.
           </>}  
@@ -39,7 +39,7 @@ export default class Holly extends React.Component {
 
           There’s a few kids in my class that remind me of myself in some way as a kid, the silent one with drive and unmatched talent, the one full of adoration for her heroes, all of them definitely have a special place in my heart in some way, even the kids who struggle, honestly, probably more so the ones that struggle at times. I am self taught in just about every skill I have. It’s life changing to see the look of joy on their faces when they finally cast a spell after hitting the floor for an hour. 
 
-          No, I am not dropping children onto the floor. They were trying to learn Feather Fall, so I let them jump from their desks for an hour. Listen, kids are malleable, they’ll be fine. 
+          No, I am not dropping children onto their heads on the floor. They were trying to learn Feather Fall, so I let them jump from their desks for an hour. Listen, kids are malleable, they’ll be fine. Besides, if I let them, these kids would jump off the build. I mean, literally more than half of them tried. So… really I think I’m doing their parent’s, and the Academy, a favor here by sticking to the desks. 
           </>}  
         />
        </div>
@@ -48,7 +48,14 @@ export default class Holly extends React.Component {
         <HollyCard 
          title="Little bit of"
          subtitle="darkness and chaos"
-         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean pharetra magna ac placerat vestibulum. Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Adipiscing at in tellus integer. Purus sit amet luctus venenatis. Malesuada bibendum arcu vitae elementum curabitur vitae. Nunc mattis enim ut tellus elementum sagittis. Vulputate mi sit amet mauris commodo quis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Nisi quis eleifend quam adipiscing vitae proin sagittis. "
+         info={<>
+          Things haven’t always been easy for me, even being a hero and a teacher aren’t things I would call easy. Actually, the opposite, to be fair and wholly honest. It’s hard, and I mess up, all the time. I will say though, I am learning to learn from those mistakes, or at least, I am really trying to be a better person, friend, and hero to everyone around me every day. Not that I can say I think I am doing a very good job but, I am doing a job at least, three of them if you count that one. I have a bit of a sordid past, even more so if you consider my less than ideal childhood, but I try to remember that my past does not define who I am today, who I want to be tomorrow, and who I hope to be in the future. It’s not easy doing the things we do, it’s not easy being a person in general but when you add power, money, magic, and the horrors of the world into the mic, it gets even more complicated and the lines get so blurry you can’t distinguish who drew them in the first place and then by then, does it matter? 
+          
+          Sorry…
+          
+          It’s been a rough few months… honestly. Still, things are looking up, I hope. 
+          
+          </>}  
         />
        </div>
 
